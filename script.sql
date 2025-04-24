955a00fedca278727994909a07265b38461777ae
Biju Mathew Thottathil <63364506+bijuthottathil@users.noreply.github.com>
2025-04-24 11:55:29 -0500

    Update script.sql

--- script.sql
+++ script.sql
@@ -1,12 +1,5 @@
 
-IF NOT EXISTS (SELECT * 
-               FROM sys.tables 
-               WHERE name = 'customer' 
-                 AND schema_id = SCHEMA_ID('dbo'))
+
 BEGIN
-    CREATE TABLE dbo.customer (
-        id INT ,
-        customername NVARCHAR(100)
-    );
 insert into dbo.customer(id,customername) values (1,'biju');
 END;
